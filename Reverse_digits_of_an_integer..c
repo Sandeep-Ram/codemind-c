25133b69fdb8f83d3ea96d3179a3e5c38b72af03
Sandeep-Ram <147791670+Sandeep-Ram@users.noreply.github.com>
2023-11-09 19:32:39 +0530

    #include<stdio.h> int main() {     int n,i=0,j;     scanf("%d",&n);
        while(n!=0) {
            j=n%10;
            i=(i*10)+j;
            n=n/10;
        }
       
            printf("%d",i);
        
    }

---
+++ Reverse_digits_of_an_integer..c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int n,i=0,j;
+    scanf("%d",&n);
+
+    while(n!=0) {
+        j=n%10;
+        i=(i*10)+j;
+        n=n/10;
+    }
+   
+        printf("%d",i);
+    
+}
