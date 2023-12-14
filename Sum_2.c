195d21f199535f5b39066bc4a4cce70d358643f3
Sandeep-Ram <147791670+Sandeep-Ram@users.noreply.github.com>
2023-12-14 11:26:39 +0530

    #include<stdio.h> int main(){     int a,b,x,y,z=0,i;     scanf("%d%d%d%d",&a,&b,&x,&y);     for(i=a;i<=b;i++){         if(i%x==0 && i%y!=0){             z=z+i;         }     }     printf("%d",z); }

---
+++ Sum_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,x,y,z=0,i;
+    scanf("%d%d%d%d",&a,&b,&x,&y);
+    for(i=a;i<=b;i++){
+        if(i%x==0 && i%y!=0){
+            z=z+i;
+        }
+    }
+    printf("%d",z);
+}
