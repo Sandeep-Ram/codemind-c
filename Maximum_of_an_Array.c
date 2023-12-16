325c27ec97546807684135053e6e3f17e471f558
Sandeep-Ram <147791670+Sandeep-Ram@users.noreply.github.com>
2023-12-16 09:48:13 +0530

    #include<stdio.h> int main() {     int n,s;     scanf("%d",&n);     int i,a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     s=a[0];     for(i=0;i<n;i++)     {         if(s<a[i])         {             s=a[i];         }     }     printf("%d",s); }

---
+++ Maximum_of_an_Array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,s;
+    scanf("%d",&n);
+    int i,a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    s=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(s<a[i])
+        {
+            s=a[i];
+        }
+    }
+    printf("%d",s);
+}
