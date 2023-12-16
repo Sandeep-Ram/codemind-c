328c79c1e05c96bdb3b4d422655892e74d8ad4e0
Sandeep-Ram <147791670+Sandeep-Ram@users.noreply.github.com>
2023-12-16 10:19:09 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int acet[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&acet[i]);     }     int sum=0;     for(i=0;i<n;i++)     {         if(i%2!=0)         sum=sum+acet[i];     } printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int acet[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&acet[i]);
+    }
+    int sum=0;
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        sum=sum+acet[i];
+    }
+printf("%d",sum);
+}
