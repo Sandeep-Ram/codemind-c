37d4ada0b9418d5361dd141b9c0d56dde9ff5a05
Sandeep-Ram <147791670+Sandeep-Ram@users.noreply.github.com>
2024-03-19 15:37:38 +0530

    #include<stdio.h> int main(){     int n,i,j,c=1;     scanf("%d",&n); int f=n;     for(i=n;i>0;i--){
           int h=1;
               for(int k=n-i;k>0;k--){
                printf(" ");
            }
            for(j=i;j>0;j--){
               
                printf("%c ",f+64);
               
            }
              f--;
       
            printf("
    ");
        c++;
    }
    }

---
+++ Pattern_7.4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,c=1;
+    scanf("%d",&n);
+int f=n;
+    for(i=n;i>0;i--){
+        
+       int h=1;
+           for(int k=n-i;k>0;k--){
+            printf(" ");
+        }
+        for(j=i;j>0;j--){
+           
+            printf("%c ",f+64);
+           
+        }
+          f--;
+   
+        printf("
+");
+    c++;
+}
+}
